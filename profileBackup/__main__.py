82ad9fc025fc980b35c64e866a29e10a87570a0f
iaso2h <iaso2h@outlook.com>
2024-06-07 23:33:37 +0800

    refactor: overhaul include and exclude mechanics. support recusive copying and counting

--- profileBackup/__main__.py
+++ profileBackup/__main__.py
@@ -1,8 +1,13 @@
+# File: profileBakup
+# Author: iaso2h
+# Description: Backup software profiles on Windows
+# Version: 0.0.2
+# Last Modified: 2024-06-07
+
 import logging
-import cli
 
 from pathlib import Path
-from backup import Exclude, Include, appDataPath, homePath
+from backup import Backup, appDataPath, homePath
 from icecream import ic
 
 # TODO: adapt user name in new desitionation
@@ -10,30 +15,33 @@ from icecream import ic
 
 
 def main():
-    threedsMax = Exclude(
+    threedsMax = Backup(
         "3ds Max",
         [
             [
                 appDataPath.glob("Local/Autodesk/3dsMax/*/*/*/UI/Workspaces"),
                 lambda parentSrcPath: parentSrcPath.parts[7][:4],
-                lambda fileSrcPath: fileSrcPath.is_dir() or str.startswith(
-                        fileSrcPath.name,
+                "exclude",
+                lambda srcPath: srcPath.is_dir() or str.startswith(
+                        srcPath.name,
                         "Workspace"
                 )
             ],
             [
                 homePath.glob("Autodesk/3ds Max*/User Settings"),
                 lambda parentSrcPath: parentSrcPath.parts[4][-4:],
+                "include",
                 lambda _: True
             ],
         ]
     )
-    autoCAD = Include(
+    autoCAD = Backup(
         "AutoCAD",
         [
             [
                 appDataPath.glob("Roaming/Autodesk/AutoCAD*/*/*"),
                 lambda parentSrcPath: parentSrcPath.parts[6][-4:],
+                "include",
                 [
                     "Plotters/Plot Styles/*.ctb",
                     "Plotters/Plot Styles/*.stb",
@@ -44,21 +52,37 @@ def main():
                 ]
             ],
             [
-                Path("C:/ProgramData/IvySoft/YSTool/Freedom"),
-                lambda _: "YSTool",
+                "C:/ProgramData/IvySoft/YSTool/Freedom",
+                "YSTool",
+                "include",
+                lambda _: True
+            ],
+            [
+                "D:/Tangent/TArchT*",
+                lambda parentSrcPath: parentSrcPath.parts[2][5:],
+                "include",
                 [
-                    "*",
+                    "SYS/*.lay",
+                    "SYS/tangent.cuix",
+                    "sys20x64/*.dwt",
+                    "sys24x64/*.dwt"
                 ]
             ],
             [
-                Path("C:/ProgramData/IvySoft/YSTool/Freedom"),
-                lambda _: "YSTool",
-                [
-                    "*",
-                ]
+                "C:/贱人工具箱*",
+                "贱人工具箱",
+                "include",
+                lambda _: True
+            ],
+            [
+                "G:\AutoCAD",
+                "Asset",
+                "include",
+                lambda _: True
             ],
         ]
     )
+
     threedsMax.backup()
     autoCAD.backup()
 
--- profileBackup/backup.py
+++ profileBackup/backup.py
@@ -1,9 +1,14 @@
+import cli
+import util
+
+ALWAYSOVERWRITE = False
+
+
 import shutil
 import logging
 import os
-import cli
 from types import GeneratorType
-from typing import Callable, Generator
+from typing import Callable, Generator, List, Union
 from pathlib import Path, PurePath
 from icecream import ic
 logger = logging.getLogger(__name__)
@@ -11,7 +16,7 @@ logger = logging.getLogger(__name__)
 # VIMRUN: g$[^#] \zslogger.debug$exe "norm gcc"$e
 
 if not cli.drive:
-    destPath    = PurePath(os.getcwd())
+    destPath = PurePath(os.getcwd(), "bk")
 else:
     destPath = PurePath("{}:/Profiles".format(cli.drive))
 os.makedirs(destPath, exist_ok=True)
@@ -21,91 +26,81 @@ appDataPath = Path("C:/Users/{}/AppData".format(userName))
 homePath    = Path("C:/Users/{}".format(userName))
 
 
+class Backup():
+    totalBackupCount = 0
+    softwareList     = []
 
-class Exclude:
     # TODO: type check https://stackoverflow.com/questions/2489669/how-do-python-functions-handle-the-types-of-parameters-that-you-pass-in
     def __init__(self, name: str, globPatterns: list):
         self.name         = name
+        type(self).softwareList.append(self.name)
+        self.softwareSequence = len(type(self).softwareList)
+
         self.globPatterns = globPatterns
-        self.backupCount  = 0
 
-    def backup(self):
-        logger.info("Backing up " + self.name + "...")
-
-        for glob in self.globPatterns:
-            # glob = [
-                # userAppDataPath.glob("Local/Autodesk/3dsMax/*/*/*/UI/Workspaces"), <function object to extract version str from the globbed path>, <function object to filter file, filter out when evaluated to True>
-            #]
-            parentGlobPath    = glob[0]
-            versionFindFunc   = glob[1]           # type: Callable
-            excludeFilterFunc = glob[2]           # type: Callable
-
-            # TODO: will raise error if parentGlobPath point to an invalid path
-            # if not isinstance(parentGlobPath, GeneratorType):
-
-            parentSrcPaths = list(parentGlobPath) # type: list[Path]
-            parentSrcPath  = ""
-            parentDstPath  = ""
-            fileSrcPath    = ""
-            fileDstPath    = ""
-
-            # Skip if no glob pattern found
-            if len(parentSrcPaths) == 0:
-                continue
-
-            for parentSrcPath in parentSrcPaths:
-                # logger.debug("  parentSrcPath: " + str(parentSrcPath))
-                versionStr = versionFindFunc(parentSrcPath)
-                logger.info("  Backing up {} {} inside: {}".format(self.name, versionStr, parentSrcPath))
-
-                parentSrcRelAnchorPath = parentSrcPath.relative_to(parentSrcPath.anchor)
-                # NOTE: versionStr can be an empty string
-                parentDstPath = Path(
-                        destPath,
-                        self.name,
-                        versionStr,
-                        parentSrcPath.anchor[:1],
-                        parentSrcRelAnchorPath
-                        )
+        self.softwareBackupCount = 0
 
 
-                for fileSrcPath in parentSrcPath.iterdir():
-                    if not excludeFilterFunc(fileSrcPath):
-                        fileSrcRelParentPath = fileSrcPath.relative_to(parentSrcPath)
-                        fileDstPath = Path(parentDstPath, fileSrcRelParentPath)
-
-                        # TODO: repeat block with Exclude class. Make it concise
-                        if fileDstPath.exists():
-                            if (fileSrcPath.stat().st_mtime - fileDstPath.stat().st_mtime) > 0:
-                                logger.info("    Backing up file: " + fileSrcPath.name)
-                                # logger.debug("    Destination: " + str(fileDstPath))
-                                shutil.copy2(fileSrcPath, fileDstPath)
-                                self.backupCount = self.backupCount + 1
-                            else:
-                                logger.info("    Skip non-modified file: " + fileSrcPath.name)
-                        else:
-                            logger.info("    Backing up file: " + fileSrcPath.name)
-                            os.makedirs(fileDstPath.parent, exist_ok=True)
-                            # TODO: Support folder
-                            shutil.copy2(fileSrcPath, fileDstPath)
-                            self.backupCount = self.backupCount + 1
-
-        logger.info("  Backed up {} {} files at\n".format(self.backupCount, self.name))
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, val):
+        if not isinstance(val, str):
+            raise ValueError("string expected")
+        self._name = val
 
 
-class Include():
-    def __init__(self, name: str, globPatterns: list):
-        self.name         = name
-        self.globPatterns = globPatterns
-        self.backupCount  = 0
+    @property
+    def globPatterns(self):
+        return self._globPatterns
+    @globPatterns.setter
+    def globPatterns(self, arg):
+        for i, globPattern in enumerate(arg):
+            if len(globPattern) != 4:
+                raise ValueError(f"The {util.idx2sequence(i)} glob pattern doesn't contain 4 elements")
+            for j, val in enumerate(globPattern):
+                # Validate path pattern
+                if j == 0:
+                    if not isinstance(val, GeneratorType) and not isinstance(val, str):
+                        raise ValueError(
+    f"Wrong given path pattern. Generator or string is expected in the {util.idx2sequence(j)} element from the {util.idx2sequence(i)} glob pattern."
+                                )
+                # Validate version string
+                elif j == 1:
+                    if not isinstance(val, Callable) and not isinstance(val, str):
+                        raise ValueError(
+    f"Wrong given version. String or function is expected in the {util.idx2sequence(j)} element from the {util.idx2sequence(i)} glob pattern."
+                                )
+                # Validate filter type.
+                elif j == 2:
+                    if not isinstance(val, str):
+                        raise ValueError("string expected")
+                    if val != "include" and val != "exclude":
+                        raise ValueError('Wrong given string value, it can only be either "include" or "exclude"')
+                # Validate filter pattern. It could be a include pattern of a exclude pattern
+                else:
+                    if not isinstance(val, Callable) and not isinstance(val, list):
+                        raise ValueError(
+    f"Wrong given filter pattern. List or function is expected in the {util.idx2sequence(j)} element from the {util.idx2sequence(i)} glob pattern."
+                                )
+                    if isinstance(val, list):
+                        for k in val:
+                            if not isinstance(k, str):
+                                raise ValueError(f"A filter string list must consist of string only.")
 
-    def copyFile(self, fileSrcPath, fileDstPath):
+        self._globPatterns = arg
+
+
+    @staticmethod
+    def copyFile(fileSrcPath: Path, fileDstPath: Path) -> int:
+        backupCount = 0
         if fileDstPath.exists():
-            if (fileSrcPath.stat().st_mtime - fileDstPath.stat().st_mtime) > 0:
+            if ALWAYSOVERWRITE or (fileSrcPath.stat().st_mtime - fileDstPath.stat().st_mtime) > 0:
                 logger.info("    Backing up file: " + fileSrcPath.name)
                 # logger.debug("    Destination: " + str(fileDstPath))
                 shutil.copy2(fileSrcPath, fileDstPath)
-                self.backupCount = self.backupCount + 1
+                backupCount = backupCount + 1
             else:
                 logger.info("    Skip non-modified file: " + fileSrcPath.name)
         else:
@@ -113,71 +108,72 @@ class Include():
             # logger.debug("    Destination: " + str(fileDstPath))
             os.makedirs(fileDstPath.parent, exist_ok=True)
             shutil.copy2(fileSrcPath, fileDstPath)
-            self.backupCount = self.backupCount + 1
+            backupCount = backupCount + 1
 
-    # TODO: Skip non-modified files
-    # TODO: dry run
-    def copyTree(self, fileSrcPath, fileDstPath):
-        logger.info("    Backing up directory: " + fileSrcPath.name)
-        # logger.debug("    Destination: " + str(fileDstPath))
-        os.makedirs(fileDstPath.parent, exist_ok=True)
-        shutil.copytree(fileSrcPath, fileDstPath, dirs_exist_ok=True)
-        count = sum(len(files) for _, _, files in os.walk(fileSrcPath))
-        self.backupCount = self.backupCount + count
+        return backupCount
+
+
+    def iterRecursive(self, parentSrcPath: Path, parentDstPath: Path, typeStr: str, filter: Union[List[str], Callable[[Path], bool]] , filterAllPathStrs: list) -> int:
+        count = 0
+
+        for srcPath in parentSrcPath.iterdir():
+            if srcPath.is_dir():
+                count = count + self.iterRecursive(srcPath, parentDstPath, typeStr, filter, filterAllPathStrs)
+            else:
+                if isinstance(filter, list):
+                    if typeStr == "exclude" and str(srcPath) in filterAllPathStrs:
+                        continue
+                    elif typeStr == "include" and str(srcPath) not in filterAllPathStrs:
+                        continue
+                    else:
+                        srcRelParentPath = srcPath.relative_to(parentSrcPath)
+                        dstPath = Path(parentDstPath, srcRelParentPath)
+                        count = count + self.copyFile(srcPath, dstPath)
+                else:
+                    if typeStr == "exclude" and filter(srcPath):
+                        continue
+                    elif typeStr == "include" and not filter(srcPath):
+                        continue
+                    else:
+                        srcRelParentPath = srcPath.relative_to(parentSrcPath)
+                        dstPath = Path(parentDstPath, srcRelParentPath)
+                        count = count + self.copyFile(srcPath, dstPath)
+
+        return count
 
 
     def backup(self):
-        logger.info("Backing up " + self.name + "...")
+        logger.info("Checking up " + self.name + "...")
 
-        for glob in self.globPatterns:
-            # glob = [
-                # userAppDataPath.glob("Local/Autodesk/3dsMax/*/*/*/UI/Workspaces"), <function object to extract version str from the globbed path>, <function object to filter file, preseve when evaluated to True>
-            #]
-            parentGlobPath        = glob[0]
-            versionFindFunc       = glob[1] # type: Callable
-            includeFileSrcRelStrs = glob[2] # type: list[str]
+        for globPattern in self.globPatterns:
+            pathPattern = globPattern[0] # type: Generator | str
+            versionFind = globPattern[1] # type: Callable | str
+            typeStr     = globPattern[2] # type: str
+            filter      = globPattern[3] # type: Callable | list
 
-            # Deal with passing a single Path object as the parent Glob Path
-            if not isinstance(parentGlobPath, GeneratorType):
-                if not parentGlobPath.exists():
-                    return
-
-                fileSrcPath = parentGlobPath.resolve()
-                fileSrcRelAnchorPath = fileSrcPath.relative_to(fileSrcPath.anchor)
-                versionStr = versionFindFunc(fileSrcPath)
-                logger.info("  Backing up {} {} inside: {}".format(self.name, versionStr, fileSrcPath))
-                # NOTE: versionStr can be an empty string
-                fileDstPath = Path(
-                        destPath,
-                        self.name,
-                        versionStr,
-                        fileSrcPath.anchor[:1],
-                        fileSrcRelAnchorPath
-                        )
-                if fileSrcPath.is_dir():
-                    self.copyTree(fileSrcPath, fileDstPath)
+            if isinstance(pathPattern, str):
+                srcPath = Path(pathPattern)
+                if not srcPath.exists():
+                    logger.info("  Skipped unfound file at: " + str(srcPath))
+                    continue
                 else:
-                    self.copyFile(fileSrcPath, fileDstPath)
+                    parentSrcPaths = [srcPath]     # type: list[Path]
+            else:
+                parentSrcPaths = list(pathPattern) # type: list[Path]
 
-                return logger.info("  Backed up {} {} {} files at: {}\n".format(self.backupCount, self.name, versionStr, fileSrcPath))
-
-
-            parentSrcPaths = list(parentGlobPath) # type: list[Path]
             parentSrcPath  = None
             parentDstPath  = None
-            fileSrcPaths   = None
             fileSrcPath    = None
             fileDstPath    = None
 
 
-            # Skip if no glob pattern found
-            if len(parentSrcPaths) == 0:
-                continue
-
             for parentSrcPath in parentSrcPaths:
                 # logger.debug("  parentSrcPath: " + str(parentSrcPath))
-                versionStr = versionFindFunc(parentSrcPath)
-                logger.info("  Backing up {} {} inside: {}".format(self.name, versionStr, parentSrcPath))
+                if isinstance(versionFind, str):
+                    versionStr = versionFind
+                else:
+                    versionStr = versionFind(parentSrcPath)
+                logger.info("  Checking up {} {} files inside: {}".format(self.name, versionStr, parentSrcPath))
 
                 parentSrcRelAnchorPath = parentSrcPath.relative_to(parentSrcPath.anchor)
                 # NOTE: versionStr can be an empty string
@@ -187,33 +183,30 @@ class Include():
                         versionStr,
                         parentSrcPath.anchor[:1],
                         parentSrcRelAnchorPath
-                        )
+                    )
 
-                for fileSrcRelStr in includeFileSrcRelStrs:
-                    if "*" in fileSrcRelStr:
-                        # Relative string contrains wildcard character
+                # Get all exclude pattern paths
+                filterAllPaths = []
+                if isinstance(filter, list):
+                    for filterPattern in filter:
+                        if not filterPattern.startswith("\\") and filterPattern.startswith("/"):
+                            filterPattern = "/" + filterPattern
 
-                        fileSrcPaths = list(parentSrcPath.glob(fileSrcRelStr))
-
-                        # Skip if no glob pattern found
-                        if len(fileSrcPaths) == 0:
+                        filterPaths = list(parentSrcPath.glob(filterPattern))
+                        if len(filterPaths) == 0:
                             continue
+                        filterAllPaths.extend(filterPaths)
 
-                        for fileSrcPath in fileSrcPaths:
-                            fileSrcRelParentPath = fileSrcPath.relative_to(parentSrcPath)
-                            fileDstPath = Path(parentDstPath, fileSrcRelParentPath)
-                            self.copyFile(fileSrcPath, fileDstPath)
-                    else:
-                        # Relative string doesn't contrains wildcard character,
-                        # but its existance need to be validated
-                        fileSrcPath = Path(parentSrcPath, fileSrcRelStr)
-                        if fileSrcPath.exists():
-                            fileSrcRelParentPath = fileSrcPath.relative_to(parentSrcPath)
-                            fileDstPath = Path(parentDstPath, fileSrcRelParentPath)
+                filterAllPathStrs = list(map(lambda p: str(p), filterAllPaths))
 
-                            if fileSrcPath.is_file():
-                                self.copyFile(fileSrcPath, fileDstPath)
-                            elif fileSrcPath.is_dir():
-                                self.copyTree(fileSrcPath, fileDstPath)
+                # Filter out path that match the excluded paths
+                parentSrcCount = self.iterRecursive(parentSrcPath, parentDstPath, typeStr, filter, filterAllPathStrs)
+                self.softwareBackupCount = self.softwareBackupCount + parentSrcCount
 
-        logger.info("  Backed up {} {} files\n".format(self.backupCount, self.name))
+        logger.info("Backed up {} {} files\n".format(self.softwareBackupCount, self.name))
+
+        # Record
+        type(self).totalBackupCount = type(self).totalBackupCount + self.softwareBackupCount
+        if self.softwareSequence == len(type(self).softwareList):
+            logger.info(f"Backed up {type(self).totalBackupCount} files from {type(self).softwareList}.\n")
+
