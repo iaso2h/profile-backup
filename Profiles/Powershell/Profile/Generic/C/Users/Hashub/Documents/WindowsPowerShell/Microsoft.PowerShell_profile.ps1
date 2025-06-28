ffa3eaf303a10de2e035ac10cb1ad4fb35e0b0e4
iaso2h <iaso2h@outlook.com>
2025-06-28 12:37:30 +0800

    chore: update

--- Profiles/Powershell/Profile/Generic/C/Users/Hashub/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Profiles/Powershell/Profile/Generic/C/Users/Hashub/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,12 @@ Invoke-Expression (&starship init powershell)
 
 # Get-ChildItemColor
 # =================
-Import-Module Get-ChildItemColor
-Set-Alias l Get-ChildItem -option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
+    Import-Module Get-ChildItemColor
+    
+    Set-Alias l Get-ChildItemColor -option AllScope
+    Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+}
 # =================
 
 # PSReadLine
