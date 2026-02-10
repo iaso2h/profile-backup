5b652687eba1c73db545372afd010d933377d050
iaso2h <mail2h@qq.com>
2026-02-10 22:22:21 +0800

    chore: add case ignore flag to regex pattern for restry matching

--- util.py
+++ util.py
@@ -91,6 +91,7 @@ def print(*args, skipChk: bool = True, **kwargs) -> None:
         convertedArgs = []
         for arg in args:
             if isinstance(arg, str):
+                re.sub(richTextPat, "", arg , re.IGNORECASE)
                 convertedArgs.append(richTextPat.sub("", arg))
             else:
                 convertedArgs.append(str(arg))
